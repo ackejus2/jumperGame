f2019699a10bdb1d930766ef995b735203085b1f
baileydiaz <baileydiaz10@gmail.com>
2022-10-19 17:14:47 -0600

    added the word list creator and rand word method

---
+++ randomWordGenerator.cs
@@ -0,0 +1,20 @@
+class wordList {
+
+    private List<string> ListOfWords = new List<string>();
+
+    private wordList(){
+        ListOfWords.Add("ghost");
+        ListOfWords.Add("treat");
+        ListOfWords.Add("candy");
+        ListOfWords.Add("haunt");
+        ListOfWords.Add("trick");
+    }
+
+    Random generator = new Random();
+    public string pull_random_word(){ 
+        int randIndex = generator.Next(ListOfWords.Count);
+        string random_word = ListOfWords[randIndex];
+        ListOfWords.RemoveAt(randIndex);
+        return random_word;
+    } 
+}
