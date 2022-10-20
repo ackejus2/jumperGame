b7582d2698e98bcf56b834c581b2ee13d8eeed9e
TrevorJac14 <trevorjac3@gmail.com>
2022-10-20 12:55:45 -0600

    somecomments for program

--- Program.cs
+++ Program.cs
@@ -3,10 +3,20 @@
 
 
     while (playerIsAlive){
+
+
         Console.WriteLine("Guess a letter [a-z]: ");
         string? user_guess = Console.ReadLine();
+
     }
+        //determine if player is crrect
+        //if they are remove letter and draw letter on screen
+        //if not then call on animation to remove a life state
+
+        //loop till word is done or out of lives
+        //if pplayer finisnhes a word end or do we want it to loop back witha play again?
+    
 }}
 
-
 string new_word = WordList.pull_random_card();
+    
