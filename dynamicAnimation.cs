c805bef0fbacee759aa83027d029ac76812fed0b
Justin Ackerman <justin.d.ackerman@gmail.com>
2022-10-20 11:00:50 -0600

    dynamicAnimation complete

--- dynamicAnimation.cs
+++ dynamicAnimation.cs
@@ -1,5 +1,43 @@
 class dynamicAnimation {
+    int missedLetters = 0;
     public void drawParachute() {
-        
+        if (missedLetters == 0) {
+            Console.WriteLine(" ___ ");
+            Console.WriteLine("/   \\");
+            Console.WriteLine(" ___ ");
+            Console.WriteLine("\\   /");
+            Console.WriteLine("\\ /");
+        }
+        else if (missedLetters == 1) {
+            Console.WriteLine("/   \\");
+            Console.WriteLine(" ___ ");
+            Console.WriteLine("\\   /");
+            Console.WriteLine("\\ /");
+        }
+        else if (missedLetters == 2) {
+            Console.WriteLine(" ___ ");
+            Console.WriteLine("\\   /");
+            Console.WriteLine("\\ /");
+        }
+        else if (missedLetters == 3) {
+            Console.WriteLine("\\   /");
+            Console.WriteLine("\\ /");
+        }
+        else if (missedLetters == 4) {
+            Console.WriteLine("\\ /");
+        }
+        else if (missedLetters == 5) {
+            Console.WriteLine("");
+        }
+    }
+    public void drawHead() {
+        var playerIsAlive = true;
+        var playerIsDead = false;
+        while (playerIsAlive){
+            Console.WriteLine("  o  ");
+        }
+        if (playerIsDead){
+            Console.WriteLine("  x  ");
+        }
     }
 }
