c33d7910c70aa83fff28a622ee285e1217cffa85
Justin Ackerman <justin.d.ackerman@gmail.com>
2022-10-22 15:53:50 -0600

    maybe finished

--- Program.cs
+++ Program.cs
@@ -1,20 +1,34 @@
 ﻿class Program{
     static void Main(string[] args){
-        var playerIsAlive = true;
         wordList randomWord = new wordList();
         string word = randomWord.pull_random_word();
         Guess newGuess = new Guess(word);
+        int missedLetters = 0;
+        dynamicAnimation dynAn = new dynamicAnimation();
+        staticAnimation statAn = new staticAnimation();
+        string oldGuessLine = "-----";
 
-        while (playerIsAlive){
+        while (missedLetters < 5){
             Console.WriteLine("Guess a letter [a-z]: ");
             string? stringGuess = Console.ReadLine();
             char charGuess = Convert.ToChar(stringGuess);
             newGuess.user_guess(charGuess);
-            string guessLine = newGuess.getUnknown();
-            Console.WriteLine(guessLine);
-            
+            string newGuessLine = newGuess.getUnknown();
+            Console.WriteLine(newGuessLine);
+            dynAn.drawParachute(missedLetters);
+            statAn.drawGround();
+            if (oldGuessLine == newGuessLine){
+                missedLetters += 1;
+            }
+            oldGuessLine = newGuessLine;
+            }
 
-    }
+        dynAn.drawParachute(missedLetters);
+        statAn.drawGround();
+        Console.Write("You died. Please try again!");
+        Console.WriteLine($"The word you missed: {word}");
+        }
+        }
         //determine if player is crrect
         //if they are remove letter and draw letter on screen
         //if not then call on animation to remove a life state
@@ -22,7 +36,7 @@
         //loop till word is done or out of lives
         //if pplayer finisnhes a word end or do we want it to loop back witha play again?
     
-}}
+
 
 
     
--- dynamicAnimation.cs
+++ dynamicAnimation.cs
@@ -1,40 +1,38 @@
 class dynamicAnimation {
-    int missedLetters = 0;
-    public void drawParachute() {
+    public void drawParachute(int missedLetters) {
         if (missedLetters == 0) {
             Console.WriteLine(" ___ ");
             Console.WriteLine("/   \\");
             Console.WriteLine(" ___ ");
             Console.WriteLine("\\   /");
             Console.WriteLine("\\ /");
+            Console.WriteLine("  o  ");
         }
         else if (missedLetters == 1) {
             Console.WriteLine("/   \\");
             Console.WriteLine(" ___ ");
             Console.WriteLine("\\   /");
             Console.WriteLine("\\ /");
+            Console.WriteLine("  o  ");
         }
         else if (missedLetters == 2) {
             Console.WriteLine(" ___ ");
             Console.WriteLine("\\   /");
             Console.WriteLine("\\ /");
+            Console.WriteLine("  o  ");
         }
         else if (missedLetters == 3) {
             Console.WriteLine("\\   /");
             Console.WriteLine("\\ /");
+            Console.WriteLine("  o  ");
         }
         else if (missedLetters == 4) {
             Console.WriteLine("\\ /");
+            Console.WriteLine("  o  ");
+            ;
         }
         else if (missedLetters == 5) {
             Console.WriteLine("");
-        }
-    }
-    public void drawHead() {
-        if (playerIsAlive){
-            Console.WriteLine("  o  ");
-        }
-        else if (!playerIsAlive){
             Console.WriteLine("  x  ");
         }
     }
